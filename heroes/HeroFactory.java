67e71cf7a94f1886a262c5a3c7b794c5a085295c
Cosmin Vancea <73666215+cvancea@users.noreply.github.com>
2019-11-30 19:54:07 +0200

    Added hero factory

---
+++ heroes/HeroFactory.java
@@ -0,0 +1,35 @@
+package heroes;
+
+public final class HeroFactory {
+    private HeroFactory() { }
+
+    public static BasicHero getHero(final HeroType heroType) {
+        switch (heroType) {
+            case KNIGHT:
+                return new Knight();
+            case PYROMANCER:
+                return new Pyromancer();
+            case ROGUE:
+                return new Rogue();
+            case WIZARD:
+                return new Wizard();
+            default:
+                return null;
+        }
+    }
+
+    public static BasicHero getHero(final char ch) {
+        switch (ch) {
+            case 'K':
+                return new Knight();
+            case 'P':
+                return new Pyromancer();
+            case 'R':
+                return new Rogue();
+            case 'W':
+                return new Wizard();
+            default:
+                return null;
+        }
+    }
+}
